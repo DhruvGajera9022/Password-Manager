0fa5220c4a31c0c6f4230c775867694170eb0e68
Dhruv Gajera <dhruvgajera05@gmail.com>
2025-06-12 15:34:01 +0530

    Add configuration files

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: './',
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      },
+    ],
+  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+    prefix: '<rootDir>/',
+  }),
+  testMatch: ['**/?(*.)+(spec|test).ts'],
+};
